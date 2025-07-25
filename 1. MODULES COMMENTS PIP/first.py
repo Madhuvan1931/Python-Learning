c91ac69d896f0cb84fea90d76039738e479b9eec
Madhuvan Singh <118827902+Madhuvan1931@users.noreply.github.com>
2025-07-25 11:26:07 +0530

    Add files via upload

---
+++ 1. MODULES COMMENTS PIP/first.py
@@ -0,0 +1,3 @@
+print("Tony Stark")
+
+print(2+3)
---
+++ 1. MODULES COMMENTS PIP/module.py
@@ -0,0 +1,13 @@
+import pyjokes as pj
+
+print("Printing Joke 1 ...")
+# This prints a random joke
+joke = pj.get_joke()
+joke1 = pj.get_joke()
+print(joke)
+print("Printing Joke 2 ...")
+print(joke1)
+"""
+this is multi 
+line comment
+"""
